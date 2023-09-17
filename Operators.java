58e5c6a6c7014f1f5581b6f1b54464fb7eae06b9
Darshan <darshanrgs45@gmail.com>
2023-09-17 22:28:16 +0530

    operators

---
+++ Operators.java
@@ -0,0 +1,23 @@
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        // assignment operator assigns value to variable
+        int c = a++;
+        int d = ++b;
+        System.out.println(c);
+        System.out.println(d);
+
+        // logical operator returns boolean
+        boolean e = a > b || a < b;
+        boolean f = a != b && b > a;
+        System.out.println(e);
+        System.out.println(f);
+
+        //ternery operator
+        String h= (a>b)? "darshan" : "raj";
+        System.out.println(h);
+
+    }
+}
