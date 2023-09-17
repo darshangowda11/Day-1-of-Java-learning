bf07be7a5c1b27cbb582e6c50b6c0cd87ea64297
Darshan <darshanrgs45@gmail.com>
2023-09-17 21:29:43 +0530

    hello world

---
+++ Hello.java
@@ -0,0 +1,18 @@
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Let's learn variables
+        int num = 45;
+        char ch = 'a';
+        boolean b = true;
+        float f = 5.6f;
+        long l = 100L;
+        short sh = 10;
+
+        // Printing all variables at once
+        System.out.println("num: " + num + ", ch: " + ch + ", b: " + b +
+                           ", f: " + f + ", l: " + l + ", sh: " + sh);
+    }
+}
